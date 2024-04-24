75bc5b222ca0c6f868ea35089f7ecb8594b3af1f
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-04-24 10:01:26 -0300

    chore: marca terminada

--- app/urls.py
+++ app/urls.py
@@ -9,10 +9,10 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
+from core.views import UserViewSet, MarcaViewSet
 
 router = DefaultRouter()
-
+router.register(r"marcas", MarcaViewSet)
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 
 urlpatterns = [
